cfc49d67687f6814aa73f6f3f07b5de3387364af
Dominik Kapusta <dominik@kapusta.cc>
2017-12-09 01:20:44 +0100

    Fix (commented out)) bitmaps imports in the example

--- printertest.py
+++ printertest.py
@@ -55,11 +55,11 @@ printer.setBarcodeHeight(100)
 printer.printBarcode("123456789123", printer.UPC_A)
 
 # # Print the 75x75 pixel logo in adalogo.py
-# import gfx.adalogo
+# import gfx.adalogo as adalogo
 # printer.printBitmap(adalogo.width, adalogo.height, adalogo.data)
 #
 # # Print the 135x135 pixel QR code in adaqrcode.py
-# import gfx.adaqrcode
+# import gfx.adaqrcode as adaqrcode
 # printer.printBitmap(adaqrcode.width, adaqrcode.height, adaqrcode.data)
 printer.println("Adafruit!")
 printer.feed(2)
